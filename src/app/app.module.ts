ecf2a19256068f558ab55094225302b5f767f64b
Alejandro <alixxantada@gmail.com>
2022-06-21 09:14:48 +0200

    feat: routing ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { RegisterComponent } from './auth/pages/register/register.component';
+import { MainComponent } from './auth/pages/main/main.component';
+import { DashboardComponent } from './protected/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
